f10dbef48bc491b5336ad4c168801e0882be9960
Lyss <shaitan.lyssm@gmail.com>
2024-07-12 15:17:34 +0200

    Add event modifiers and string utils

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,3 @@
 // Reexport your entry components here
 export * from './actions/index.js'
+export * from './utils/index.js'
---
+++ src/lib/utils/index.ts
@@ -0,0 +1,17 @@
+// export * from './uuid';
+// export * from './context';
+// export { formatXml } from './xml.js';
+export * from './string.js';
+export * from './eventListeners.js';
+
+export { v4 as uuidv4 } from 'uuid';
+import {v4 as uuid} from 'uuid'
+let idCount = 0;
+export function newLocalId(baseName?: string) {
+	idCount += 1;
+	return `${baseName ?? 'local-unique-id'}-${idCount}}`;
+}
+
+export function newUuid(baseName?: string) {
+	return `${baseName ? baseName + '-' : ''}${uuid()}`;
+}
