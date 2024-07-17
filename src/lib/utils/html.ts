b01bad7bec000fe4a109940e2cc7f371f816ebcc
Lyss <shaitan.lyssm@gmail.com>
2024-07-17 20:30:07 +0200

    Add downloads

--- src/lib/utils/html.ts
+++ src/lib/utils/html.ts
@@ -9,3 +9,30 @@ export function posFromClient({
 }): Position {
 	return { x, y };
 }
+
+export function download(filename: string, data: unknown) {
+	const text = typeof data === 'string' ? data : JSON.stringify(data, undefined, 4);
+	const blob = new Blob([text], { type: 'application/octet-stream' });
+	const url = URL.createObjectURL(blob);
+	const a = document.createElement('a');
+	a.href = url;
+	a.download = filename;
+	document.body.appendChild(a);
+	a.click();
+	URL.revokeObjectURL(url);
+	document.body.removeChild(a);
+
+}
+
+export function downloadJSON(name: string, data: unknown) {
+	const json = JSON.stringify(data, undefined, 4);
+	const blob = new Blob([json], { type: 'application/json' });
+	const url = URL.createObjectURL(blob);
+	const a = document.createElement('a');
+	a.href = url;
+	a.download = `${name}.json`;
+	document.body.appendChild(a);
+	a.click();
+	URL.revokeObjectURL(url);
+	document.body.removeChild(a);
+}
