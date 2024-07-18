e7608075e76abc3458b4f8259676afcec7fa661a
Lyss <shaitan.lyssm@gmail.com>
2024-07-18 22:31:40 +0200

    Try to fix wasm exports

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config';
 import wasmPack from 'vite-plugin-wasm-pack'
 
 export default defineConfig({
-	plugins: [sveltekit(), wasmPack('./selenite-commons-rs')],
+	plugins: [sveltekit(), wasmPack('./selenite-commons-rs', ['selenite-commons-rs'])],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
