5c424fc5176f9c325f1df5b29cf4932fba6b8a98
Lyss <shaitan.lyssm@gmail.com>
2024-07-22 20:44:09 +0200

    Make horizontal scroll smooth

--- src/lib/actions/scroll.ts
+++ src/lib/actions/scroll.ts
@@ -1,15 +1,30 @@
-import type { Action } from "svelte/action";
+import type { Action } from 'svelte/action';
+import { tweened } from 'svelte/motion';
+import { get } from 'svelte/store';
 
-export const horizontalScroll: Action = (node) => {
-    const handleWheel = (e: WheelEvent) => {
-        if (e.deltaY === 0) return;
-        node.scrollLeft += e.deltaY;
-        e.preventDefault();
-    };
-    node.addEventListener("wheel", handleWheel, { passive: false });
-    return {
-        destroy() {
-        node.removeEventListener("wheel", handleWheel);
-        },
-    };
-}
+export const horizontalScroll: Action<HTMLElement, { duration?: number } | undefined> = (
+	node,
+	{ duration = 150 } = {}
+) => {
+	let scroll = tweened(node.scrollLeft, {
+		duration,
+		interpolate: (a, b) => (t) => a + (b - a) * t
+	});
+
+	scroll.subscribe((value) => {
+		node.scrollLeft = value;
+	});
+
+	const handleWheel = (e: WheelEvent) => {
+		if (e.deltaY === 0) return;
+		scroll.set(get(scroll) + e.deltaY);
+		e.preventDefault();
+	};
+
+	node.addEventListener('wheel', handleWheel, { passive: false });
+	return {
+		destroy() {
+			node.removeEventListener('wheel', handleWheel);
+		}
+	};
+};
---
+++ src/routes/scroll/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import {horizontalScroll} from '$lib'
+	import { range } from 'lodash-es';
+    </script>
+
+<div class="w-32 flex gap-2 overflow-x-auto" use:horizontalScroll>
+    {#each range(100) as i}
+    <span>{i}</span>
+
+    {/each}
+</div>
