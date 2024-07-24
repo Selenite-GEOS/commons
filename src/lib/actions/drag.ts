fb3bd25d11a15cc9e716c8e6e73ce50b6ed0ad3a
Lyss <shaitan.lyssm@gmail.com>
2024-07-24 17:32:23 +0200

    Add clone only on drag

--- src/lib/actions/drag.ts
+++ src/lib/actions/drag.ts
@@ -14,9 +14,9 @@ export type DragItemOptions = Omit<DragOptions, 'bounds' | 'transform' | 'recomp
 	items: unknown[];
 	/** Duration of flip animation. */
 	flipDuration: number;
-	/** 
-	 * Min distance between items swaps. 
-	 * 
+	/**
+	 * Min distance between items swaps.
+	 *
 	 * After doing an item swap, the next swap will be ignored if the distance
 	 * between the last swap and the current position is less than this value.
 	 */
@@ -27,7 +27,7 @@ export type DragItemOptions = Omit<DragOptions, 'bounds' | 'transform' | 'recomp
  * Action to make an item in an array of items draggable.
  *
  * The array will be mutated to update the order.
- * 
+ *
  * Options : {@link DragItemOptions}.
  */
 export const draggableItem: Action<HTMLElement, DragItemOptions> = (node, params) => {
@@ -37,24 +37,27 @@ export const draggableItem: Action<HTMLElement, DragItemOptions> = (node, params
 	let base: Position = { x: rect.x, y: rect.y };
 	let baseOffset: Position = { x: 0, y: 0 };
 	/** Offset at last item swap. */
-	let lastSwapOffset: Position |undefined;
+	let lastSwapOffset: Position | undefined;
 	let waitForFlip = false;
 	let currentParams = params;
 	let numSwaps = 0;
-	node.classList.add('selenite-drag')
+
+	// Add a class to keep track of draggable items
+	node.classList.add('selenite-drag');
+
 	/**
 	 * Returns the bounds of the draggable item.
-	 * 
+	 *
 	 * It is computed by getting the union of the bounds of all draggable items.
 	 */
 	function getBounds() {
-		console.debug("Compute bounds.")
+		console.debug('Compute bounds.');
 		const draggables = Array.from(node.parentElement!.querySelectorAll('.selenite-drag'));
 		return getBoundsUnion(draggables);
 	}
 
 	let bounds = getBounds();
-	
+
 	const dragOptions: DragOptions = {
 		...params,
 		bounds: {
@@ -73,7 +76,7 @@ export const draggableItem: Action<HTMLElement, DragItemOptions> = (node, params
 		},
 		recomputeBounds: {
 			dragStart: true,
-			drag: true,
+			drag: true
 		},
 		// Apply transform to clone
 		transform({ offsetX, offsetY, rootNode }) {
@@ -84,24 +87,8 @@ export const draggableItem: Action<HTMLElement, DragItemOptions> = (node, params
 
 		async onDragStart(data) {
 			params.onDragStart?.(data);
-			
-			await tick()
 
-			// Create clone of node.
-			const rect = node.getBoundingClientRect();
-			base = { x: rect.x, y: rect.y };
-
-			clone = node.cloneNode(true) as HTMLElement;
-			clone.classList.remove('neodrag');
-			clone.classList.remove('selenite-drag');
-			const parent = node.parentElement as HTMLElement;
-			node.insertAdjacentElement('beforebegin', clone);
-			
-			clone.style.position = 'fixed';
-			clone.style.pointerEvents = 'none';
-			clone.style.left = `${base.x}px`;
-			clone.style.top = `${base.y}px`;
-			clone.style.visibility = 'hidden';
+			await tick();
 			bounds = getBounds();
 			if (draggableReturn) {
 				draggableReturn.update?.({ ...dragOptions, bounds });
@@ -109,10 +96,29 @@ export const draggableItem: Action<HTMLElement, DragItemOptions> = (node, params
 		},
 
 		async onDrag(data) {
+			if (!clone) {
+				// Create clone of node
+				const rect = node.getBoundingClientRect();
+				base = { x: rect.x, y: rect.y };
+
+				clone = node.cloneNode(true) as HTMLElement;
+				clone.classList.remove('neodrag');
+				clone.classList.remove('selenite-drag');
+
+				clone.style.position = 'fixed';
+				clone.style.zIndex = '10';
+				clone.style.pointerEvents = 'none';
+				clone.style.left = `${base.x}px`;
+				clone.style.top = `${base.y}px`;
+				clone.style.visibility = 'hidden';
+
+				// Insert clone before node
+				node.insertAdjacentElement('beforebegin', clone);
+			}
+
 			params.onDrag?.(data);
-			
+
 			if (waitForFlip) return;
-			if (!clone) return;
 			clone.style.pointerEvents = 'auto';
 			document.body.style.pointerEvents = 'none';
 			clone.style.visibility = 'visible';
@@ -121,11 +127,10 @@ export const draggableItem: Action<HTMLElement, DragItemOptions> = (node, params
 
 			clone.style.cursor = 'grabbing';
 
-			
-			const offset: Position = {x: data.offsetX, y: data.offsetY}
+			const offset: Position = { x: data.offsetX, y: data.offsetY };
 			if (
 				lastSwapOffset &&
-				getDistance(lastSwapOffset, offset) < (params.minSwapDistance ?? (clone.clientWidth / 2))
+				getDistance(lastSwapOffset, offset) < (params.minSwapDistance ?? clone.clientWidth / 2)
 			) {
 				console.debug('Too close from last swap');
 				return;
@@ -140,8 +145,8 @@ export const draggableItem: Action<HTMLElement, DragItemOptions> = (node, params
 			if (currentIndex === closestIndex) return;
 			console.debug(numSwaps++, 'Move from', currentIndex, 'to', closestIndex);
 			params.items.splice(closestIndex, 0, params.items.splice(currentIndex, 1)[0]);
-			clone.remove()
-			
+			clone.remove();
+
 			lastSwapOffset = offset;
 			waitForFlip = true;
 			setTimeout(() => {
@@ -156,7 +161,11 @@ export const draggableItem: Action<HTMLElement, DragItemOptions> = (node, params
 			baseOffset = { x: data.offsetX, y: data.offsetY };
 			document.body.style.cursor = '';
 			document.documentElement.style.cursor = '';
-			clone?.remove();
+
+			if (clone) {
+				clone.remove();
+				clone = undefined;
+			}
 			node.style.opacity = '1';
 			params.onDragEnd?.(data);
 		}
