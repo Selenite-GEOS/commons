5d41d0920b8fa10df94ddecd51e63e1c34ec9a88
MoonLyss <shaitan.lyssm@gmail.com>
2024-08-21 13:24:35 +0200

    Add animationFrame promise

--- src/lib/utils/promise.ts
+++ src/lib/utils/promise.ts
@@ -1,17 +1,27 @@
 /**
  * Utils for working with promises.
- * 
+ *
  * Promises are used in JavaScript to handle asynchronous operations. They are a way to handle the result of an asynchronous operation once it completes.
- * 
+ *
  * You can await the completion of a promise using the `await` keyword. This will pause the execution of the current function until the promise resolves.
  * @module
  */
 
 /**
  * Returns a promise that resolves after a delay.
- * @param ms - the number of miliseconds to wait before resolving the promise.
+ * @param ms - the number of miliseconds to wait before resolving the promise. Defaults to 0.
  * @returns Promise that resolves after the specified delay.
  */
-export function sleep(ms: number) {
-    return new Promise((r) => setTimeout(r, ms));
-} 
+export function sleep(ms?: number) {
+	return new Promise((r) => setTimeout(r, ms ?? 0));
+}
+
+export function animationFrame(n?: number): Promise<void> {
+	return new Promise(r => {
+		let count = n ?? 1;
+		while (count > 0) {
+            requestAnimationFrame(() => count--);
+        }
+        r();
+	});
+}
