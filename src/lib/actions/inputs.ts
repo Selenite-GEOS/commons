21fda228dd9ca23a983c2847e850c88348606e80
MoonLyss <shaitan.lyssm@gmail.com>
2024-08-21 20:26:18 +0200

    Add parameter to autosize

--- src/lib/actions/inputs.ts
+++ src/lib/actions/inputs.ts
@@ -1,14 +1,22 @@
 import type { Action } from 'svelte/action';
 import baseAutosize from 'autosize';
 
-export const autosize: Action<HTMLTextAreaElement> = (textarea) => {
-	baseAutosize(textarea);
 
-	// $effect.root()
+/**
+ * Action to make a textarea autosize.
+ * 
+ * A parameter can be passed to the action to trigger an update when it changes.
+ * @param {HTMLTextAreaElement} textarea - The textarea element to make autosize.
+ */
+export const autosize: Action<HTMLTextAreaElement, unknown | undefined> = (textarea) => {
+	baseAutosize(textarea);
 
 	return {
 		destroy() {
 			baseAutosize.destroy(textarea);
+		},
+		update() {
+			baseAutosize.update(textarea);
 		}
 	};
 };
---
+++ src/routes/input/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { autosize } from '$lib';
+
+	let content = $state('Text');
+
+	$effect(() => {
+		const interval = setInterval(addRandomContent, 100);
+		return () => clearInterval(interval);
+	});
+
+	function addRandomContent() {
+		content += ' ' + Math.random().toString(36).substring(7);
+	}
+</script>
+<h1 class="text-2xl font-bold mx-auto">Textarea Autosize</h1>
+<div class="grid justify-center items-center">
+	<textarea use:autosize={content} bind:value={content} class="w-[20rem] textarea textarea-bordered textarea-primary"></textarea>
+</div>
