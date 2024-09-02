16556e1d18dea8faa715e84c93a2a5da49f03eed
MoonLyss <shaitan.lyssm@gmail.com>
2024-09-02 15:10:31 +0200

    Ignore type error in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import wasmPack from 'vite-plugin-wasm-pack';
 export default defineConfig({
+	// @ts-expect-error Ignore type error
 	plugins: [sveltekit(), wasmPack([], '@selenite/commons-rs')],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}'],
