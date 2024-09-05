8e28d183a67213a7480165a80921c990afeab5a7
MoonLyss <shaitan.lyssm@gmail.com>
2024-09-05 15:02:03 +0200

    Fix document listener

--- src/lib/actions/document.ts
+++ src/lib/actions/document.ts
@@ -1,28 +1,37 @@
 import type { Action } from 'svelte/action';
 
 export type DocumentParams = {
-    [key in keyof DocumentEventMap]?: (event: DocumentEventMap[key]) => void;
+	[key in keyof DocumentEventMap]?: (event: DocumentEventMap[key]) => void;
 } & AddEventListenerOptions;
+
+/**
+ * Action to listen to events on the document.
+ * @param node node the action is bound to
+ * @param params the listeners and the listeners options combined
+ * @returns svelte action
+ */
 export const documentListener: Action<Element, DocumentParams> = (node, params: DocumentParams) => {
 	function setup() {
 		Object.keys(params).forEach((key) => {
-			// @ts-expect-error Ignore type error
-			document.addEventListener(key, params[key], params);
+            const p = params as Record<string, EventListener | boolean>;
+            if (typeof p[key] !== "function") return;
+			document.addEventListener(key, p[key], params);
 		});
 	}
-    setup()
+	setup();
 
 	return {
 		destroy() {
 			Object.keys(params).forEach((key) => {
-				// @ts-expect-error Ignore type error
-				document.removeEventListener(key, params[key], params);
+                const p = params as Record<string, EventListener | boolean>;
+                if (typeof p[key] !== 'function') return;
+				document.removeEventListener(key, p[key], params);
 			});
 		},
 		update(parameter) {
-            this.destroy?.()
-            params = parameter;
-            setup()
-        },
+			this.destroy?.();
+			params = parameter;
+			setup();
+		}
 	};
 };
