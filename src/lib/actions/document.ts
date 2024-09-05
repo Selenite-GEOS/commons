c019897292baa955c06ba7ce92b644e643096ef6
MoonLyss <shaitan.lyssm@gmail.com>
2024-09-05 14:51:37 +0200

    Fix types

--- src/lib/actions/document.ts
+++ src/lib/actions/document.ts
@@ -1,6 +1,8 @@
 import type { Action } from 'svelte/action';
 
-export type DocumentParams = DocumentEventMap & AddEventListenerOptions;
+export type DocumentParams = {
+    [key in keyof DocumentEventMap]: (event: DocumentEventMap[key]) => void;
+} & AddEventListenerOptions;
 export const documentListener: Action<Element, DocumentParams> = (node, params: DocumentParams) => {
 	function setup() {
 		Object.keys(params).forEach((key) => {
