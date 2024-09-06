57e3d87868b601055a70817938cdcf45082b1865
MoonLyss <shaitan.lyssm@gmail.com>
2024-09-06 13:54:02 +0200

    Add selenite logo

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,2 +1,3 @@
 export {default as MatchHighlighter} from './MatchHighlighter.svelte';
 export {default as PathGenerator} from './PathGenerator.svelte';
+export {default as SeleniteLogo} from './SeleniteLogo.svelte'
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import '../app.css';
-	import { capitalizeWords } from '$lib';
+	import { capitalizeWords, SeleniteLogo } from '$lib';
 	let {children} = $props();
 	const title = $derived(capitalizeWords($page.route.id?.slice(1).split('-').join(' ') ?? ''));
 </script>
 
+<SeleniteLogo class="absolute m-2"/>
 <main class="p-4 min-h-screen grid justify-center gap-2 items-start place-content-start">
 	{#if title}
 	<a class="m-auto mb-2" href="/"><btn class="btn w-fit btn-sm">Main Page</btn></a>
