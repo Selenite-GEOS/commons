57f7f231f94af95bbc91ef71e2172ac1131cc547
MoonLyss <shaitan.lyssm@gmail.com>
2024-09-06 17:30:38 +0200

    Fix missing export

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,3 +1,4 @@
 export {default as MatchHighlighter} from './MatchHighlighter.svelte';
 export {default as PathGenerator} from './PathGenerator.svelte';
 export {default as SeleniteLogo} from './SeleniteLogo.svelte'
+export {default as Tags} from './Tags.svelte';
