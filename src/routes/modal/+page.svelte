b942bf752ada7dee1ecac38455c8798540187d29
MoonLyss <shaitan.lyssm@gmail.com>
2025-05-01 18:29:27 +0200

    Add modal page

---
+++ src/routes/modal/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { Modal } from "$lib";
+
+    function onclick() {
+        Modal.instance.show({snippet: hello});
+    }
+</script>
+
+{#snippet hello()}
+    <h1>Hello, World!</h1>
+    <p>This is a modal.</p>  
+{/snippet}
+
+<button class="btn" {onclick}>Open</button>
